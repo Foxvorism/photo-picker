9de3a96102b7487c32f469431d8a01d0710daa2a
Foxvorism <imjohani2005@gmail.com>
2026-09-02 15:21:53 +0700

    Add cursor-pointer class to button components for better UX

--- app/components/FilterSelectedButton.vue
+++ app/components/FilterSelectedButton.vue
@@ -31,7 +31,7 @@ function toggleFilter() {
           ? 'bg-[#083182]! text-[#d0dbee]! dark:bg-[#d0dbee]! dark:text-[#083182]! outline-0!'
           : ''
       "
-      class="fixed bottom-13.5 right-3 z-40 rounded-full bg-[#d0dbee] text-[#083182] outline-2 outline-[#083182] shadow-xl shadow-[#083182]/25 transition hover:bg-[#083182] hover:text-[#d0dbee] focus-visible:ring-[#083182]/40 disabled:cursor-not-allowed disabled:opacity-50 dark:bg-[#083182] dark:text-[#d0dbee] dark:outline-[#d0dbee] dark:hover:bg-[#d0dbee] dark:hover:text-[#083182] dark:focus-visible:ring-[#d0dbee]/50 md:bottom-5 md:right-16"
+      class="cursor-pointer fixed bottom-13.5 right-3 z-40 rounded-full bg-[#d0dbee] text-[#083182] outline-2 outline-[#083182] shadow-xl shadow-[#083182]/25 transition hover:bg-[#083182] hover:text-[#d0dbee] focus-visible:ring-[#083182]/40 disabled:cursor-not-allowed disabled:opacity-50 dark:bg-[#083182] dark:text-[#d0dbee] dark:outline-[#d0dbee] dark:hover:bg-[#d0dbee] dark:hover:text-[#083182] dark:focus-visible:ring-[#d0dbee]/50 md:bottom-5 md:right-16"
       color="neutral"
       :disabled="disabled"
       size="xl"
